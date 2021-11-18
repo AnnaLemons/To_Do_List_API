aeb1dd28c8e0a5c27e5ee190e908e07eb1dae179
AnnaLemons <a.lopezdelg@gmail.com>
2021-11-18 14:58:12 +0000

    saved

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+
+from models import db, Task
+from sqlalchemy import exc
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +33,69 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+    #GET TASK
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/task', methods=['GET'])
+def get_task():
+    tasks= Task.get_all()
+    if tasks:
+        return jsonify(tasks),200
 
-    return jsonify(response_body), 200
+    return jsonify({'msg':'No task available'}), 404
+
+    #GET TASK BY ID
+
+@app.route('/task/<int:id>', methods=['GET'])
+def get_task_by_id(id):
+    task= Task.get_by_id(id)
+
+    if task:
+        return jsonify(task.to_dict()), 200
+
+    return jsonify({'error': 'Task is not found'}), 404
+
+    #CREATE "TO DO"
+
+@app.route('/task', methods=['POST'])
+def create_to_do():
+    new_to_do=request.json.get('item',None)
+
+    if not new_to_do:
+        return jsonify({'error':'Missing task'}), 400
+
+    task= Task(to_do=new_to_do, done=False)
+    try:
+        task_created=task.create()
+        return jsonify(task_created.to_dict()), 201
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in data'}), 400
+
+    #UPDATE "TO DO"
+
+@app.route('/task/<int:id>', methods=['PUT','PATCH'])
+def update_to_do(id):
+    new_to_do=request.json.get('item', None)
+
+    if not new_todo:
+        return jsonify({'error': 'Missing task'}), 400
+
+    task= Task.get_by_id(id)
+    if task:
+        task=task.update(new_to_do)
+        return jsonify(task.to_dict()), 200
+
+    return jsonify({'error': 'Task is not found'}), 404
+
+    #DELETE "TO DO"
+
+@app.route('/task/<int:id>', methods=['DELETE'])
+def delete_to_do(id):
+    task=Task.get_by_id(id)
+    if task:
+        task.delete()
+        return jsonify(task.to_dict()), 200
+
+    return jsonify({'error':'Task is not found'}), 404
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    to_do = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def repr(self):
+        return f'Task is :{self.todo}, done:{self.done}, id:{self.id}'
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "to_do": self.to_do,
+            "done": self.done
         }
+
+    @classmethod
+    def get_by_id(cls,id_task):
+        task= cls.query.filter_by(id=id_task).one_or_none()
+        return task
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def get_all(cls):
+        tasks= cls.query.all()
+        return [task.to_dict() for task in tasks]
+
+    def update(self, todo):
+        self.to_do=to_do
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
